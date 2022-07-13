9ae54680002b66fe54400f558efadc19228d11e2
RohitKS7 <rohitkumarsuman2011india@gmail.com>
2022-07-13 15:53:46 +0530

    1 file modified

--- src/hooks/useReducer.jsx
+++ src/hooks/useReducer.jsx
@@ -1,19 +1,35 @@
 import React, { useState, useReducer } from 'react';
 import './style.css';
 
-// UseReducer hook ->
+// UseReducer hook -> this hook works same like useState but best for complex projects
+
+// useReducer also returns 2 objects in a array same as useState but useReducer takes 2 arguments
+// state and initialdata works same like useState hook -> initialdata = initialData and state = currentData
+// but dispatch is not used to update the state, instead it used to trigger the "reducer" function
+// and that reducer funciton will be the one who update the state of data.
+
+// Reducer funciton -> it also takes 2 parameters -> currentData and action
+const reducer = (state, action) => {
+  if (action.type === 'INCR') {
+    state = state + 1;
+  }
+  if (state > 0 && action.type === 'DECR') {
+    state = state - 1;
+  }
+  return state;
+};
 
 const UseReducer = () => {
-  // const [myNum, setMyNum] = useState(0);
-
-  // useReducer also returns 2 objects in a array same as useState but useReducer takes 2 arguments
+  const initialData = 10;
+  // useReducer hook
   const [state, dispatch] = useReducer(reducer, initialData);
 
   return (
     <>
       <div className="center_div">
-        <p>{myNum}</p>
-        <div class="button2" onClick={() => setMyNum(myNum + 1)}>
+        <p>{state}</p>
+        {/* dispatch is used in onClick funciton */}
+        <div className="button2" onClick={() => dispatch({ type: 'INCR' })}>
           <span></span>
           <span></span>
           <span></span>
@@ -21,11 +37,7 @@ const UseReducer = () => {
           {/* number increment */}
           INCR
         </div>
-        {/* ternary operator used => if myNum ki value greater than 0 hai, tho decrement karo vrna 0 karo  */}
-        <div
-          class="button2"
-          onClick={() => (myNum > 0 ? setMyNum(myNum - 1) : setMyNum(0))}
-        >
+        <div className="button2" onClick={() => dispatch({ type: 'DECR' })}>
           <span></span>
           <span></span>
           <span></span>
